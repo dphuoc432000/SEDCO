bf8b17f5b27eec9d8025b018799dad4b746ae63d
Hieu Pham <winhieu2000@gmail.com>
2021-10-13 16:20:49 +0700

    hieu update

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -4,7 +4,8 @@ import {withRouter}from "react-router-dom";
 // import Home from "../../pages/Home/Home"
 import Map from './Map/Map.js'
 import Status from './Status/Status.js'
-import './MainContent.css'
+// import './MainContent.css'
+import '../../styles/main.css'
 class Main extends React.Component {
     render() {
         return (

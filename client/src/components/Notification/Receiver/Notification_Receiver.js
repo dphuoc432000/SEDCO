06a71bb9ae8eefad9114009ff63f8fdaa5a72ee8
Hieu Pham <winhieu2000@gmail.com>
2021-11-21 23:23:45 +0700

    hieu update

--- client/src/components/Notification/Receiver/Notification_Receiver.js
+++ client/src/components/Notification/Receiver/Notification_Receiver.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router";
-import "./notification.css";
+import "../notification.css";
 class Notification_Receiver extends Component {
   render() {
     return (
